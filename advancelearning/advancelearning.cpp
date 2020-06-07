b1e459767525f32352bc902ebac0f7c6de1d26f8
Upkar <dahiyaupkar@gmail.com>
2020-06-06 19:57:49 -0700

    Use copy constructor to copy/duplicate above values.

--- advancelearning/advancelearning.cpp
+++ advancelearning/advancelearning.cpp
@@ -11,6 +11,11 @@ public:
 		setLength(l);		//Why I call the setLength and setBreadth function: 
 		setBreadth(b);		//Reason: By default, I am passing Length and Breadth as a zero value to avoid mishandling of data.
 	}
+	Rectangle(Rectangle(&r)) // I use copy constructor to copy/duplicate above values.
+	{
+		length = r.length;
+		breadth = r.breadth;
+	}
 	//Mutator
 	void setLength(int l)
 	{
@@ -43,7 +48,9 @@ public:
 int main()
 {
 	Rectangle r1(25,10);
+	Rectangle r2(r1);
 	cout << "Area of rectangle is : " << r1.area() << endl;
 	cout << "Perimeter of rectangle is : " << r1.perimeter() << endl;
-	return 0;
+
+	cout << "Copy of above values: "<<"\nArea :" << r2.area()<<" Perimeter :"<<r2.perimeter()<<endl;
 }
